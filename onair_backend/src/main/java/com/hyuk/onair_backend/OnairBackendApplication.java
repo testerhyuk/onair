c7b157139be2dd6179ce26423a92bf7cd148d6a2
Hyuk <testerhyuk214@gmail.com>
2025-06-23 17:05:48 +0900

    initial commit

---
+++ onair_backend/src/main/java/com/hyuk/onair_backend/OnairBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hyuk.onair_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnairBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnairBackendApplication.class, args);
+	}
+
+}
